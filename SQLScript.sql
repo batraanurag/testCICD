d743fc66d0c1c670fac8868cc3322e5f4d7bbb50
batraanurag <32878833+batraanurag@users.noreply.github.com>
2019-05-07 14:23:19 -0400

    Create SQLScript.sql

---
+++ SQLScript.sql
@@ -0,0 +1,12 @@
+drop table testAzureDevops;
+
+Create table testAzureDevops
+( id number(10) NOT NULL,  
+  item varchar2(50) NOT NULL,  
+  comment varchar2(500)  
+); 
+
+insert into testAzureDevops(id,item,comment) VALUES (1,'beef and cheddar', 'beef sandwich at Arbys');
+insert into testAzureDevops(id,item,comment) VALUES (2,'rueben', 'another beef sandwich at Arbys');
+insert into testAzureDevops(id,item,comment) VALUES (3,'hot wings', 'wings at BDubs');
+insert into testAzureDevops(id,item,comment) VALUES (4,'mild wings', 'another set of wings at BDubs');
