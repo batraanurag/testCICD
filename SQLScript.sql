77301cb505c77a646a908170a9d57d6f368bfe86
batraanurag <32878833+batraanurag@users.noreply.github.com>
2019-05-10 10:58:53 -0400

    Update SQLScript.sql

--- SQLScript.sql
+++ SQLScript.sql
@@ -3,11 +3,11 @@ drop table testAzureDevops;
 Create table testAzureDevops
 ( id number(10) NOT NULL,  
   item varchar2(50) NOT NULL,  
-  comment varchar2(500)  
+  comments varchar2(500)  
 ); 
 
-insert into testAzureDevops(id,item,comment) VALUES (1,'beef and cheddar', 'beef sandwich at Arbys');
-insert into testAzureDevops(id,item,comment) VALUES (2,'rueben', 'another beef sandwich at Arbys');
-insert into testAzureDevops(id,item,comment) VALUES (3,'hot wings', 'wings at BDubs');
-insert into testAzureDevops(id,item,comment) VALUES (4,'mild wings', 'another set of wings at BDubs');
-insert into testAzureDevops(id,item,comment) VALUES (5,'another wings', 'another different set of wings at BDubs');
+insert into testAzureDevops(id,item,comments) VALUES (1,'beef and cheddar', 'beef sandwich at Arbys');
+insert into testAzureDevops(id,item,comments) VALUES (2,'rueben', 'another beef sandwich at Arbys');
+insert into testAzureDevops(id,item,comments) VALUES (3,'hot wings', 'wings at BDubs');
+insert into testAzureDevops(id,item,comments) VALUES (4,'mild wings', 'another set of wings at BDubs');
+insert into testAzureDevops(id,item,comments) VALUES (5,'another wings', 'another different set of wings at BDubs');
